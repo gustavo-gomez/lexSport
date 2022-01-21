6d625fe41bdadb888e67ff15bf2e4feedb64161a
Gustavo Gomez <gomezf09@gmail.com>
2022-01-20 23:17:27 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+
+const PORT = process.env.PORT || 9000
+const PROD = process.env.NODE_ENV === 'production'
+
+const app = express();
+if (PROD){
+	app.use(express.static(__dirname + '/client'))
+}
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/client/index.html')
+})
+
+// app.use('/', authentication)
+// app.use('/timezones', timezones)
+// app.use('/users', users)
+
+
+
+if (PROD) {
+	// Handles any requests that don't match the ones above
+	app.get('*', (req, res) => {
+		res.sendFile(__dirname + '/client/index.html')
+	})
+}
+
+app.listen(PORT, () => {
+	console.log(`Server listening on port: ${PORT}`)
+})
+
+
