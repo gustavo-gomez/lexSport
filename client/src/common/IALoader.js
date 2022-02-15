f4c6436bdec866194cd21c0cec06cb1860b2223c
Gustavo Gomez <gomezf09@gmail.com>
2022-02-14 21:46:12 -0500

    new empty lottie

--- client/src/common/IALoader.js
+++ client/src/common/IALoader.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import Lottie from 'lottie-react';
+import React from 'react'
+import Lottie from 'lottie-react'
 import loader from '../images/lottie/loader.json'
+import empty from '../images/lottie/empty.json'
 import '../scss/components/ialoader.scss'
 
-const IALoader = () => {
+export const LOTTIE_TYPE = {
+	EMPTY: {
+		source: empty,
+		text: 'No hay registros',
+		height: 150,
+	},
+	LOADING: {
+		source: loader,
+		text: 'Cargando...',
+		height: 250,
+	}
+}
+
+const IALoader = ({ type }) => {
 
 	return (
 		<div
 			className="ia-loader-container"
+			style={type.source === empty ? { position: 'relative' } : { position: 'absolute', zIndex: 9999 }}
 		>
-
 			<Lottie
-				className={'ia-loader'}
-				animationData={loader}
+				className={type.source === empty ? 'ia-lottie': 'ia-loader'}
+				animationData={type.source}
 				loop
 				autoplay
 			/>
+			<p>{type.text}</p>
 		</div>
 	)
 }
 
 export default IALoader
+
+IALoader.defaultProps = {
+	type: LOTTIE_TYPE.LOADING,
+}
