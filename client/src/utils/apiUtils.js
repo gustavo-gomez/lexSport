3f546146fafa59ab74d0af9412b83018ad286c3f
Gustavo Gomez <gomezf09@gmail.com>
2022-02-17 00:28:01 -0500

    APIs

--- client/src/utils/apiUtils.js
+++ client/src/utils/apiUtils.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-import {AUTH_TOKEN_KEY} from "../slices/authSlice";
+import { AUTH_TOKEN_KEY } from '../slices/authSlice'
 
 const BASE_URL = process.env.REACT_APP_LEX_SPORT_IP_BACKEND || 'http://192.168.18.6:9000'
 
@@ -8,24 +8,55 @@ export const ENDPOINTS = {
 	LOGIN: '/login',
 	WORKERS: '/workers',
 	PRODUCTS: '/products',
+	ACTIVITY: '/activity',
 }
 
 export const loginAPI = async (user, password) => {
-	return await callAPI('post', BASE_URL + ENDPOINTS.LOGIN, {user, password})
+	return await callAPI('post', BASE_URL + ENDPOINTS.LOGIN, { user, password })
 }
 
 // Get all costureras
 export const getAllCosturerasAPI = async () => {
-	return await callAPI('get', BASE_URL + ENDPOINTS.WORKERS)
+	return await callAPI('GET', BASE_URL + ENDPOINTS.WORKERS)
 }
+
+// create costurera
+export const createCostureraAPI = async (worker) => {
+	return await callAPI('POST', BASE_URL + ENDPOINTS.WORKERS, { worker })
+}
+
+// update costurera
+export const updateCostureraAPI = async (id, worker) => {
+	return await callAPI('PUT', `${BASE_URL}${ENDPOINTS.WORKERS}/${id}`, { worker })
+}
+
 
 // Get all products
 export const getAllProductsAPI = async () => {
-	return await callAPI('get', BASE_URL + ENDPOINTS.PRODUCTS)
+	return await callAPI('GET', BASE_URL + ENDPOINTS.PRODUCTS)
+}
+
+// create costurera
+export const createProductAPI = async (product) => {
+	return await callAPI('POST', BASE_URL + ENDPOINTS.PRODUCTS, { product })
+}
+
+// update costurera
+export const updateProductAPI = async (id, product) => {
+	return await callAPI('PUT', `${BASE_URL}${ENDPOINTS.PRODUCTS}/${id}`, { product })
+}
+
+// new activities
+export const newActivitiesAPI = async (dateMillis, activities) => {
+	return await callAPI('POST', `${BASE_URL}${ENDPOINTS.ACTIVITY}`, { dateMillis, activities })
+}
+
+// get activities
+export const loadActivitiesAPI = async (starDate, endDate) => {
+	return await callAPI('GET', `${BASE_URL}${ENDPOINTS.ACTIVITY}?startDate=${starDate}&endDate=${endDate}`)
 }
 
 const callAPI = async (method, url, body) => {
-
 	const authToken = localStorage.getItem(AUTH_TOKEN_KEY)
 	const config = {
 		method,
@@ -37,10 +68,10 @@ const callAPI = async (method, url, body) => {
 	}
 
 	try {
-		const {data} = await axios(url, config)
+		const { data } = await axios(url, config)
 		return data
-	} catch (error) {
-		console.log('error: ', error?.response);
+	} catch ( error ) {
+		console.log('error: ', error?.response)
 		return {
 			isError: true,
 			responseCode: error?.response?.status,
