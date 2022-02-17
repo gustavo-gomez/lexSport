393e7823fc07cfedb225bc4d07a8ceb5008b0a69
Gustavo Gomez <gomezf09@gmail.com>
2022-02-17 00:13:38 -0500

    header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,45 @@
 import React from 'react'
 import '../scss/components/header.scss'
-import {useDispatch, useSelector} from 'react-redux'
-import {auth, logout} from "../slices/authSlice";
-import {toggleDrawer, generalSettings} from "../slices/generalSettingsSlice";
-import {useNavigate} from "react-router-dom";
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import HelpIcon from '@mui/icons-material/Help';
-import IconButton from '@mui/material/IconButton';
-import Link from '@mui/material/Link';
-import MenuIcon from '@mui/icons-material/Menu';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import Tab from '@mui/material/Tab';
-import Tabs from '@mui/material/Tabs';
-import Toolbar from '@mui/material/Toolbar';
-import Tooltip from '@mui/material/Tooltip';
-import Typography from '@mui/material/Typography';
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../slices/authSlice'
+import { generalSettings, toggleDrawer } from '../slices/generalSettingsSlice'
+import { useNavigate } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import AppBar from '@mui/material/AppBar'
+import IconButton from '@mui/material/IconButton'
+import MenuIcon from '@mui/icons-material/Menu'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import useScrollTrigger from '@mui/material/useScrollTrigger'
+import Slide from '@mui/material/Slide'
 
 const lightColor = 'rgba(255, 255, 255, 0.7)';
 const drawerWidth = 240;
+
+function HideOnScroll(props) {
+	const { children, window } = props;
+	// Note that you normally won't need to set the window ref as useScrollTrigger
+	// will default to window.
+	// This is only being set here because the demo is in an iframe.
+	const trigger = useScrollTrigger({
+		target: window ? window() : undefined,
+	});
+
+	return (
+		<Slide appear={false} direction="down" in={!trigger}>
+			{children}
+		</Slide>
+	);
+}
+
+HideOnScroll.propTypes = {
+	children: PropTypes.element.isRequired,
+	/**
+	 * Injected by the documentation to work in an iframe.
+	 * You won't need it on your project.
+	 */
+	window: PropTypes.func,
+};
 
 const Header = () => {
 	// const {loggedUser: {first_name, last_name, role_admin}} = useSelector(auth)
@@ -37,30 +55,32 @@ const Header = () => {
 
 	return (
 		<React.Fragment>
-			<AppBar
-				color="primary"
-				position="sticky"
-				sx={{
-					width: { sm: `calc(100% - ${drawerWidth}px)` },
-					ml: { sm: `${drawerWidth}px` },
-				}}
-				elevation={0}
-			>
-				<Toolbar>
-					<IconButton
-						color="inherit"
-						aria-label="open drawer"
-						edge="start"
-						onClick={() => dispatch(toggleDrawer())}
-						sx={{ mr: 2, display: { sm: 'none' } }}
-					>
-						<MenuIcon />
-					</IconButton>
-					<Typography variant="h6" noWrap component="div">
-						Responsive drawer
-					</Typography>
-				</Toolbar>
-			</AppBar>
+			<HideOnScroll>
+				<AppBar
+					color="primary"
+					position="sticky"
+					sx={{
+						width: {sm: `calc(100% - ${drawerWidth}px)`},
+						ml: {sm: `${drawerWidth}px`},
+					}}
+					elevation={0}
+				>
+					<Toolbar>
+						<IconButton
+							color="inherit"
+							aria-label="open drawer"
+							edge="start"
+							onClick={() => dispatch(toggleDrawer())}
+							sx={{ mr: 2, display: { sm: 'none' } }}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography variant="h6" noWrap component="div">
+							Responsive drawer
+						</Typography>
+					</Toolbar>
+				</AppBar>
+			</HideOnScroll>
 		</React.Fragment>
 	);
 }
