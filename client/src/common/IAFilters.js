3fc13675a6362b97d0214ddd9f419087d1365198
Gustavo Gomez <gomezf09@gmail.com>
2022-02-20 23:07:15 -0500

    add select at filters

--- client/src/common/IAFilters.js
+++ client/src/common/IAFilters.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import IATimeDatePicker from './IATimeDatePicker'
 import '../scss/components/iafilters.scss'
 import Button from '@mui/material/Button'
+import IATextInput from './IATextInput'
+import IASelect from './IASelect'
+import isEmpty from 'lodash/isEmpty'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllCostureras, workers } from '../slices/workersSlice'
 
-const IAFilters = ({ startDate, setStartDate, endDate, setEndDate, onSearch, isLoading }) => {
+const IAFilters = ({ onSearch, isLoading, showWorkerFilter, defaultStartDate }) => {
+	const { workerList } = useSelector(workers)
+	const [isError, setIsError] = useState(false)
+	const [startDate, setStartDate] = useState(defaultStartDate || new Date())
+	const [endDate, setEndDate] = useState(new Date())
+	const [selectedWorker, setSelectedWorker] = useState(null)
+
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		dispatch(getAllCostureras())
+	}, [])
+
+
+	console.log('showWorkerFilter: ', showWorkerFilter)
+	const search = () => {
+		if (showWorkerFilter && isEmpty(selectedWorker)) {
+			setIsError(true)
+		} else {
+			onSearch({ startDate, endDate, workerId: selectedWorker?.id })
+		}
+	}
+
+	console.log('selectedWorker', selectedWorker)
 
 	return (
 		<div
@@ -12,6 +40,30 @@ const IAFilters = ({ startDate, setStartDate, endDate, setEndDate, onSearch, isL
 			<div
 				className={'filters-wrapper'}
 			>
+				{
+					showWorkerFilter &&
+					<div
+						className={'filter-item'}
+					>
+						<IASelect
+							label={'Costurera'}
+							key={`worker-filter`}
+							value={workerList?.find(worker => worker.id === selectedWorker?.id)}
+							data={workerList}
+							textToShow={(object) => {
+								return `${object?.firstName}, ${object?.lastName}`
+							}}
+							onChange={(object) => {
+								setIsError(false)
+								setSelectedWorker(object)
+							}}
+							isRequired
+							error={isError}
+							// helperText={isError ? 'Campo requerido' : ''}
+							variant={'outlined'}
+						/>
+					</div>
+				}
 				<div
 					className={'filter-item'}
 				>
@@ -37,7 +89,7 @@ const IAFilters = ({ startDate, setStartDate, endDate, setEndDate, onSearch, isL
 			</div>
 			<>
 				<Button
-					onClick={onSearch}
+					onClick={search}
 					variant="outlined"
 					disabled={isLoading}
 				>
@@ -49,3 +101,7 @@ const IAFilters = ({ startDate, setStartDate, endDate, setEndDate, onSearch, isL
 }
 
 export default IAFilters
+
+IAFilters.defaultProps = {
+	showWorkerFilter: false
+}
