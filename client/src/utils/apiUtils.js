862ba8c33c1326c0b709ba9176dad984c6f802be
Gustavo Gomez <gomezf09@gmail.com>
2022-11-20 16:17:39 -0500

    testing

--- client/src/utils/apiUtils.js
+++ client/src/utils/apiUtils.js
@@ -2,8 +2,8 @@ import axios from 'axios'
 import { AUTH_TOKEN_KEY } from '../slices/authSlice'
 import isEmpty from 'lodash/isEmpty'
 
-const BASE_URL = process.env.REACT_APP_LEX_SPORT_IP_BACKEND || 'http://192.168.18.6:9000'
-// const BASE_URL = 'https://ilterflores.com'
+// const BASE_URL = process.env.REACT_APP_LEX_SPORT_IP_BACKEND || 'http://192.168.18.6:9000'
+const BASE_URL = 'http://164.92.64.212:9000'
 
 
 export const ENDPOINTS = {
@@ -18,92 +18,6 @@ export const ENDPOINTS = {
 
 export const loginAPI = async (user, password) => {
 	return await callAPI('post', BASE_URL + ENDPOINTS.LOGIN, { user, password })
-}
-
-// Get all workers
-export const getAllWorkersAPI = async (roles) => {
-	return await callAPI('GET', `${BASE_URL}${ENDPOINTS.WORKERS}${roles?.length > 0 ? `?roles=${roles?.join(',')}` : ''}`)
-}
-
-// create costurera
-export const createCostureraAPI = async (worker) => {
-	return await callAPI('POST', BASE_URL + ENDPOINTS.WORKERS, { worker })
-}
-
-// update costurera
-export const updateCostureraAPI = async (id, worker) => {
-	return await callAPI('PUT', `${BASE_URL}${ENDPOINTS.WORKERS}/${id}`, { worker })
-}
-
-
-// Get all products
-export const getAllProductsAPI = async () => {
-	return await callAPI('GET', BASE_URL + ENDPOINTS.PRODUCTS)
-}
-
-// create costurera
-export const createProductAPI = async (product) => {
-	return await callAPI('POST', BASE_URL + ENDPOINTS.PRODUCTS, { product })
-}
-
-// update costurera
-export const updateProductAPI = async (id, product) => {
-	return await callAPI('PUT', `${BASE_URL}${ENDPOINTS.PRODUCTS}/${id}`, { product })
-}
-
-// new activities
-export const newActivitiesAPI = async (dateMillis, activities) => {
-	return await callAPI('POST', `${BASE_URL}${ENDPOINTS.ACTIVITY}`, { dateMillis, activities })
-}
-
-// get activities
-export const loadActivitiesAPI = async (starDate, endDate, workerId) => {
-	return await callAPI('GET', `${BASE_URL}${ENDPOINTS.ACTIVITY}?startDate=${starDate}&endDate=${endDate}${!isEmpty(workerId) ? `&workerId=${workerId}` : ''}`)
-}
-
-// delete activities
-export const deleteActivityAPI = async (id) => {
-	return await callAPI('DELETE', `${BASE_URL}${ENDPOINTS.ACTIVITY}/${id}`)
-}
-
-// delete activities
-export const editActivityAPI = async (activity) => {
-	return await callAPI('PUT', `${BASE_URL}${ENDPOINTS.ACTIVITY}`, { activity })
-}
-
-// get products dashboard
-export const loadProductsDashboardAPI = async (starDate, endDate) => {
-	return await callAPI('GET', `${BASE_URL}${ENDPOINTS.PRODUCTS_DASHBOARD}?startDate=${starDate}&endDate=${endDate}`)
-}
-// get workers dashboard
-export const loadWorkersDashboardAPI = async (starDate, endDate, workerId) => {
-	return await callAPI('GET', `${BASE_URL}${ENDPOINTS.PRODUCTS_WORKERS}/${workerId}?startDate=${starDate}&endDate=${endDate}`)
-}
-
-// delete product
-export const deleteProductAPI = async (id) => {
-	return await callAPI('DELETE', `${BASE_URL}${ENDPOINTS.PRODUCTS}/${id}`)
-}
-
-// delete costurera
-export const deleteCostureraAPI = async (id) => {
-	return await callAPI('DELETE', `${BASE_URL}${ENDPOINTS.WORKERS}/${id}`)
-}
-
-
-//new schedules
-export const newSchedulesAPI = async (schedules) => {
-	return await callAPI('POST', `${BASE_URL}${ENDPOINTS.SCHEDULES}`, { schedules })
-}
-
-// get schedules
-export const loadSchedulesAPI = async (starDate, endDate, workerId) => {
-	return await callAPI('GET', `${BASE_URL}${ENDPOINTS.SCHEDULES}?startDate=${starDate}&endDate=${endDate}${!isEmpty(workerId) ? `&workerId=${workerId}` : ''}`)
-}
-
-// update schedule
-export const updateSchedulesAPI = async (schedule) => {
-	return await callAPI('PUT', `${BASE_URL}${ENDPOINTS.SCHEDULES}`, {schedule})
 }
 
 const callAPI = async (method, url, body) => {
--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
 import authentication from "./routes/authentication"
+import { collectDefaultMetrics, register } from 'prom-client';
+
+collectDefaultMetrics();
+
 
 const PORT = process.env.PORT || 9000
 const PROD = process.env.NODE_ENV === 'production'
@@ -17,6 +21,17 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/client/index.html')
 })
+
+app.get('/metrics', async (_req, res) => {
+	try {
+		res.set('Content-Type', register.contentType);
+		console.log("metricssssssssss")
+		res.end(await register.metrics());
+	} catch (err) {
+		res.status(500).end(err);
+	}
+});
+
 
 app.use('/', authentication)
 
