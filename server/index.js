d531ec9126e46b1b15bac66032e42273a4f52a8b
Gustavo Gomez <gomezf09@gmail.com>
2022-11-20 17:02:18 -0500

    testing

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
-import authentication from "./routes/authentication"
-import { collectDefaultMetrics, register } from 'prom-client';
+import authentication from './routes/authentication'
+import client  from 'prom-client'
 
-collectDefaultMetrics();
+const collectDefaultMetrics = client.collectDefaultMetrics
+collectDefaultMetrics({timeout: 5000})
+
+
+const counter = new client.Counter({
+	name: 'node_request_operation_total',
+	help: 'Total number of requests',
+})
+const histogram = new client.Histogram({
+	name: 'node_request_duration_seconds',
+	help: 'Histogram of request duration',
+	buckets: [1, 2, 5, 6, 10]
+})
 
 
 const PORT = process.env.PORT || 9000
@@ -16,21 +28,25 @@ if (PROD) {
 }
 app.use(cors())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
 
 app.get('/', (req, res) => {
+	counter.inc()
 	res.sendFile(__dirname + '/client/index.html')
 })
 
 app.get('/metrics', async (_req, res) => {
 	try {
-		res.set('Content-Type', register.contentType);
-		console.log("metricssssssssss")
-		res.end(await register.metrics());
-	} catch (err) {
-		res.status(500).end(err);
+		res.set('Content-Type', register.contentType)
+		console.log('metricssssssssss')
+		res.end(await register.metrics())
+	} catch ( err ) {
+		res.status(500).end(err)
 	}
-});
+})
 
 
 app.use('/', authentication)
